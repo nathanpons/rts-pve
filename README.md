4e19c8f5bd5bbe153c39a825cb282633ff37996a
PapsBurr <npons3932@gmail.com>
2026-03-25 22:01:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# What is this?
+This project contains basic functionality for 2D character controllers with a fully functional example.
+
+The repository includes its own state machine system which will be documented soon<sup>tm</sup>.
+
+There are two branches of node classes building the system, states and transitions, I've done my best to make them self explanatory and tried adding a good amount of quality of life features in the editor itself.
+
+Have fun making games!
---
+++ scripts/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 50.0
+# const JUMP_VELOCITY = -300.0
+
+func _physics_process(_delta: float) -> void:
+	# Add the gravity.
+	# if not is_on_floor():
+		# velocity += get_gravity() * delta
+
+	# Handle jump.
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		# velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	var horizontal_direction := Input.get_axis("move_left", "move_right")
+	var vertical_direction := Input.get_axis("move_up", "move_down")
+	if horizontal_direction:
+		velocity.x = horizontal_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if vertical_direction:
+		velocity.y = vertical_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
