a387a6ba3242a8786103a0e7f99095286097511f
PapsBurr <npons3932@gmail.com>
2026-05-01 12:21:00 -0600

    feat: add unit script

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -1,42 +1,18 @@
-@tool
-class_name MyUnit
-extends Node2D
+extends CharacterBody2D
 
+@export var speed = 100
+var target = null
 
-@onready var hit_area = $Area2D
-@export var selection_color := Color(1, 1, 0)
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("set_target"):
+		target = get_global_mouse_position()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	# Create collision and make it centered
-	var rect = RectangleShape2D.new()
-	rect.size = hit_area.body_shape.size
-	hit_area.shape = rect
-	
-	# Make collision shape centered on Area2D
-	hit_area.body_shape = rect
-	# Note: Area2D expects a CollisionShape2D
-	# If you are using a Texture, the Area2D shape is defined by TextureRect
-	# For simplicity, let's use a basic rectangle.
-	hit_area.body_shape = RectangleShape2D.new()
-	hit_area.body_shape.size = Vector2(20, 20)
-	
-func _on_hit_area_body_entered(body: RigidBody2D) -> void:
-	# This is mainly for physics. For selection, we ignore this
-	# But if you use the Area2D for collision detection, you can toggle selection here
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func highlight(color: Color) -> void:
-	hit_area.modulate = color
-
-func dehighlight() -> void:
-	hit_area.modulate = selection_color
-
-func is_in_selection_area(min_pos: Vector2, max_pos: Vector2) -> bool:
-	var unit_position = hit_area.global_position
-	return min_pos.x < unit_position.x and unit_position.x < max_pos.x and \
-		   min_pos.y < unit_position.y and unit_position.y < max_pos.y
+func _physics_process(delta: float) -> void:
+	if target != null:
+		velocity = position.direction_to(target)
+	velocity = velocity.normalized() * speed
+	move_and_collide(velocity * delta)
+	if velocity != Vector2.ZERO:
+		$AnimationPlayer.play("walking")
+	else:
+		$AnimationPlayer.play("idle")
