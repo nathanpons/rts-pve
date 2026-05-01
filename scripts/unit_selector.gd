323e5a9442fbd038fee37c02b26217f5355b5b99
PapsBurr <npons3932@gmail.com>
2026-05-01 12:21:10 -0600

    feat: add unit selector script

---
+++ scripts/unit_selector.gd
@@ -0,0 +1,42 @@
+@tool
+class_name MyUnit
+extends Node2D
+
+
+@onready var hit_area = $Area2D
+@export var selection_color := Color(1, 1, 0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Create collision and make it centered
+	var rect = RectangleShape2D.new()
+	rect.size = hit_area.body_shape.size
+	hit_area.shape = rect
+	
+	# Make collision shape centered on Area2D
+	hit_area.body_shape = rect
+	# Note: Area2D expects a CollisionShape2D
+	# If you are using a Texture, the Area2D shape is defined by TextureRect
+	# For simplicity, let's use a basic rectangle.
+	hit_area.body_shape = RectangleShape2D.new()
+	hit_area.body_shape.size = Vector2(20, 20)
+	
+func _on_hit_area_body_entered(body: RigidBody2D) -> void:
+	# This is mainly for physics. For selection, we ignore this
+	# But if you use the Area2D for collision detection, you can toggle selection here
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func highlight(color: Color) -> void:
+	hit_area.modulate = color
+
+func dehighlight() -> void:
+	hit_area.modulate = selection_color
+
+func is_in_selection_area(min_pos: Vector2, max_pos: Vector2) -> bool:
+	var unit_position = hit_area.global_position
+	return min_pos.x < unit_position.x and unit_position.x < max_pos.x and \
+		   min_pos.y < unit_position.y and unit_position.y < max_pos.y
