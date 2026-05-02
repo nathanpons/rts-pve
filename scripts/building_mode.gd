394d0e0a29230224f6e8e5e476b271e57b4d5a65
PapsBurr <npons3932@gmail.com>
2026-05-02 14:50:40 -0600

    feat: add building_mode scene and script for detecting if terrain is buildable

---
+++ scripts/building_mode.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@export var building_area := Rect2(0, 0, 16, 16)
+var building_area_color_success = Color.GREEN
+var building_area_color_error = Color.RED
+var query_rect = RectangleShape2D.new()
+var can_build = []
+
+func _ready() -> void:
+  pass
+
+func _process(delta: float) -> void:
+  building_area.position = get_global_mouse_position() - building_area.size / 2
+  var space = get_world_2d().direct_space_state
+  query_rect.extents = abs(building_area.size) / 2
+  var q = PhysicsShapeQueryParameters2D.new()
+  q.shape = query_rect
+  q.collision_mask = 2
+  q.transform = Transform2D(0, get_global_mouse_position())
+  can_build = space.intersect_shape(q)
+  queue_redraw()
+
+func _draw() -> void:
+  if can_build.size() == 0:
+    draw_rect(building_area, building_area_color_success)
+  else:
+    draw_rect(building_area, building_area_color_error)
