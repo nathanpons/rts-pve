baa9349bdd05928f9da1bb9a2e74ea862d485453
PapsBurr <npons3932@gmail.com>
2026-05-01 21:50:06 -0600

    feat: add logic for selecting and controlling units

--- scripts/unit_selector.gd
+++ scripts/unit_selector.gd
@@ -4,12 +4,18 @@ var dragging = false
 var drag_start = Vector2.ZERO
 var selection_rect = RectangleShape2D.new()
 var selection_color := Color.AQUA
+var selected = []
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			dragging = true
-			drag_start = get_global_mouse_position()
+			if selected.size() == 0:
+				dragging = true
+				drag_start = get_global_mouse_position()
+			else:
+				for item in selected:
+					item.collider.selected = false
+				selected = []
 		elif dragging:
 			dragging = false
 			queue_redraw()
@@ -20,8 +26,18 @@ func _unhandled_input(event: InputEvent) -> void:
 			q.shape = selection_rect
 			q.collision_mask = 2
 			q.transform = Transform2D(0, (drag_end + drag_start) / 2)
+			selected = space.intersect_shape(q)
+			for item in selected:
+				item.collider.selected = true
+
 	if event is InputEventMouseMotion and dragging:
 		queue_redraw()
+
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
+		if event.pressed:
+			for item in selected:
+				item.collider.target = get_global_mouse_position()
+
 
 func _draw() -> void:
 	if dragging:
