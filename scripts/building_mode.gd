166e45a0a2691d12abe195c5927c77f9f90dc868
PapsBurr <npons3932@gmail.com>
2026-05-04 13:48:18 -0600

    refactor: change spaces to tabs to fit gdscript style guidelines

--- scripts/building_mode.gd
+++ scripts/building_mode.gd
@@ -7,21 +7,21 @@ var query_rect = RectangleShape2D.new()
 var can_build = []
 
 func _ready() -> void:
-  pass
+	pass
 
 func _process(delta: float) -> void:
-  building_area.position = get_global_mouse_position() - building_area.size / 2
-  var space = get_world_2d().direct_space_state
-  query_rect.extents = abs(building_area.size) / 2
-  var q = PhysicsShapeQueryParameters2D.new()
-  q.shape = query_rect
-  q.collision_mask = 2
-  q.transform = Transform2D(0, get_global_mouse_position())
-  can_build = space.intersect_shape(q)
-  queue_redraw()
+	building_area.position = get_global_mouse_position() - building_area.size / 2
+	var space = get_world_2d().direct_space_state
+	query_rect.extents = abs(building_area.size) / 2
+	var q = PhysicsShapeQueryParameters2D.new()
+	q.shape = query_rect
+	q.collision_mask = 2
+	q.transform = Transform2D(0, get_global_mouse_position())
+	can_build = space.intersect_shape(q)
+	queue_redraw()
 
 func _draw() -> void:
-  if can_build.size() == 0:
-    draw_rect(building_area, building_area_color_success)
-  else:
-    draw_rect(building_area, building_area_color_error)
+	if can_build.size() == 0:
+		draw_rect(building_area, building_area_color_success)
+	else:
+		draw_rect(building_area, building_area_color_error)
