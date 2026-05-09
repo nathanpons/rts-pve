367e37f9d39e1b4df68c226e0220ddb036916538
PapsBurr <npons3932@gmail.com>
2026-05-08 21:35:38 -0600

    feat: add template for scripts

---
+++ script_templates/node/basic_custom_script.gd
@@ -0,0 +1,25 @@
+# meta-name: Basic Custom Script
+# meta-description: Base script with structures
+# meta-default: true
+# meta-space-indent: 4
+extends Node
+## signals
+## enums
+## consts
+## exports
+## public vars
+## private vars
+## onready vars
+## built-in override methods
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+## public methods
+
+## private methods
