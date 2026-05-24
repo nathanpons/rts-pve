9f2c66b00a825ba5d8aafefb98af775b35136184
PapsBurr <npons3932@gmail.com>
2026-05-23 19:59:17 -0600

    feat: create and pass attack data though signal

--- scripts/components/melee_attack_component.gd
+++ scripts/components/melee_attack_component.gd
@@ -1,6 +1,8 @@
 class_name MeleeAttackComponent
 extends Node2D
 
+signal attack_started(attack, target)
+
 @export var attack_damage: int = 10
 @export var attack_range: float = 30.0
 @export var attack_cooldown: float = 1.0
@@ -8,10 +10,11 @@ extends Node2D
 
 var _last_attack_time: float = 0.0
 var _attack_timer: Timer = null
-# Target for attack
+var attack_object = load("uid://bv3tc5dkx7in") # attack.gd
+var possible_targets = []
 var target: Node = null
 
-@onready var attack_shape: CollisionShape2D = self.get_node("AttackShape")
+@onready var attack_shape: CollisionShape2D = get_node("AttackShape")
 
 
 func _ready() -> void:
@@ -23,9 +26,13 @@ func _ready() -> void:
 	_attack_timer.autostart = false
 
 	# Attack shape and range
-	if attack_shape.shape is CircleShape2D:
-		attack_shape.shape.radius = attack_range
+	if attack_shape:
+		if attack_shape.shape is CircleShape2D:
+			attack_shape.shape.radius = attack_range
+			print("Attack shape set!")
 
+	else:
+		print("Could not find attack_shape.")
 
 func attack() -> void:
 	var current_time = Time.get_ticks_usec() / 1000.0
@@ -45,7 +52,8 @@ func attack() -> void:
 func _perform_melee_attack(target_node: Unit) -> void:
 	if target_node:
 		if target_node.team != self.team:
-			pass
+			var attack_data = attack_object.new()
+			attack_started.emit(attack_data)
 
 
 func _on_attack_timeout() -> void:
@@ -60,4 +68,3 @@ func set_target(node: Node) -> void:
 
 func clear_target() -> void:
 	target = null
-
