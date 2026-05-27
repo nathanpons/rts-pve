a269364720866fce0bf82f44877e460fac53566f
PapsBurr <npons3932@gmail.com>
2026-05-27 15:00:13 -0600

    fix: attack cooldown logic

--- scripts/components/melee_attack_component.gd
+++ scripts/components/melee_attack_component.gd
@@ -4,23 +4,24 @@ extends Node2D
 @export var attack_damage: int = 10
 @export var attack_range: float = 20.0
 @export var attack_cooldown: float = 1.0
-@export var is_debug: bool = false
 
-var _attack_timer: Timer = null
+var _attack_cooldown_timer: Timer = null
 var attack_object = load("uid://bv3tc5dkx7in") # attack.gd
 var possible_targets = []
 var target: Node = null
 
+@onready var node_name = self.get_parent().name
 @onready var attack_shape: CollisionShape2D = get_node("AttackShape")
 
 
 func _ready() -> void:
 	# Attack Timer and cooldown management
-	_attack_timer = Timer.new()
-	add_child(_attack_timer)
-	_attack_timer.timeout.connect(_on_attack_timeout)
-	_attack_timer.wait_time = attack_cooldown
-	_attack_timer.autostart = false
+	_attack_cooldown_timer = Timer.new()
+	_attack_cooldown_timer.name = "AttackTimer"
+	add_child(_attack_cooldown_timer)
+	_attack_cooldown_timer.timeout.connect(_on_attack_timeout)
+	_attack_cooldown_timer.wait_time = attack_cooldown
+	_attack_cooldown_timer.autostart = false
 
 	# Attack shape and range
 	if attack_shape:
@@ -34,7 +35,7 @@ func _ready() -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	# Add target to possible targets
-	if (!possible_targets.has(area)):
+	if not possible_targets.has(area):
 		possible_targets.append(area)
 
 		# Check if on separate teams
@@ -53,19 +54,21 @@ func _on_area_exited(area: Area2D) -> void:
 
 
 func _on_attack_timeout() -> void:
-	if _attack_timer:
-		_attack_timer.wait_time = attack_cooldown
-		attack()
-		_attack_timer.start()
+	print(node_name + " attack timer ran out!")
+	if not _attack_cooldown_timer:
+		_attack_cooldown_timer = Timer.new()
+	print(node_name + " setting attack timer")
+	_attack_cooldown_timer.wait_time = attack_cooldown
+	attack()
 
 
 func _perform_melee_attack(target_area: Area2D) -> void:
 	if target_area:
-		var attack_data = 10.0
-		# var target_hitbox = target.get_node("HitboxComponent")
+		var attack_data = 10.0 # Temporary attack data
 		if is_instance_valid(target) and target.has_method("take_damage"):
-			print(self.get_parent().name + " Attacking target: " + str(target.get_parent().name))
+			print(node_name + " is attacking target: " + str(target.get_parent().name))
 			target.take_damage(attack_data)
+			_attack_cooldown_timer.start()
 		else:
 			print("No take_damage method found on target: " + str(target.name))	
 
@@ -73,13 +76,17 @@ func _perform_melee_attack(target_area: Area2D) -> void:
 func attack() -> void:
 	# Check if target is valid and in range
 	if target == null:
+		print(node_name + "'s target is null. Cancelling attack.")
 		return
 	
+	# Check if attack is on cooldown
+	if not _attack_cooldown_timer.is_stopped():
+		print(node_name + " cannot attack! Attack still on cooldown!")
 	_perform_melee_attack(target)
 
 
 func set_target() -> void:
-	if (!possible_targets.is_empty()):
+	if not possible_targets.is_empty():
 		target = possible_targets[0]
 		print("Target set to: " + str(target))
 		attack()
