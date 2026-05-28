ae27013a555c78f679bd796e5c0d749ba815402a
PapsBurr <npons3932@gmail.com>
2026-05-28 13:35:00 -0600

    feat: add health bar to units

--- scripts/components/health_component.gd
+++ scripts/components/health_component.gd
@@ -1,16 +1,26 @@
 class_name HealthComponent
 extends Node2D
 
-@export var MAX_HEALTH := 100
+@export var min_health: float = 0.0
+@export var max_health: float = 100.0
+@export var health_bar: ProgressBar
 var health: float
 
 
 func _ready() -> void:
-	health = MAX_HEALTH
+	health = max_health
+	health_bar.min_value = min_health
+	health_bar.max_value = max_health
+	health_bar.value = health
+
+
+func _process(_delta: float) -> void:
+	rotation = -owner.rotation
 
 
 func take_damage(damage_amount: float):
 	health -= damage_amount
+	health_bar.value = health
 	print("Health: " + str(self.health))
 
 	if health <= 0:
