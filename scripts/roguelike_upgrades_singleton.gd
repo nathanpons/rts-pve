6e53a77b349fc0b257dde20844976c34425a697d
PapsBurr <npons3932@gmail.com>
2026-06-13 13:17:48 -0600

    build: add sqlite plugin to the project

---
+++ scripts/roguelike_upgrades_singleton.gd
@@ -0,0 +1,22 @@
+extends Node
+## signals
+## enums
+## consts
+## exports
+## public vars
+## private vars
+## onready vars
+## built-in override methods
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+## public methods
+
+## private methods
