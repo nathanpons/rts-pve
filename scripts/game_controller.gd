c0dd9a4b5c5f336c5909fc6bf56b2d9522eb04c8
PapsBurr <npons3932@gmail.com>
2026-06-12 21:55:35 -0600

    feat: add card selection to game script

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -2,21 +2,43 @@
 class_name GameController
 extends Node2D
 
-@export var selection_color := Color(1, 1, 0)
-@export var deselection_color := Color(0, 0, 0)
-@export var highlight_modulate := Color(0.5, 1, 0, 0.5)
-@export var units: Array[Node]
 
-# Track the mouse down position for drag selection
-@export var mouse_down_pos := Vector2()
+var is_card_selection = false
+var card_selection_popup = preload("uid://kc05cxcj7ery")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Assume we have a "Units" container or get them dynamically
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		if not is_card_selection:
+			open_card_selection_popup()
+			is_card_selection = true
+		else:
+			close_card_selection_popup()
+			is_card_selection = false
+
+
+func open_card_selection_popup() -> void:
+	print("open_card_selection_popup called")
+	var game_objects = get_children()
+	if "card_selection_popup" not in game_objects:
+		var card_selection_popup_instance = card_selection_popup.instantiate()
+		card_selection_popup_instance.name = "card_selection_popup"
+		add_child(card_selection_popup_instance)
+
+
+func close_card_selection_popup() -> void:
+	print("close_card_selection_popup called")
+	var game_objects = get_children()
+	for child in game_objects:
+		if child.name == "card_selection_popup":
+			child.queue_free()
