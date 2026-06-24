3cd1c8fc02dd66bdaad70ca07f583f922a34d59d
PapsBurr <npons3932@gmail.com>
2026-06-24 15:14:54 -0600

    refactor: add boilerplate

--- scripts/ant_unit.gd
+++ scripts/ant_unit.gd
@@ -1,1 +1,21 @@
 extends Unit
+## signals
+## enums
+## consts
+## exports
+## public vars
+## private vars
+## onready vars
+
+
+## built-in override methods
+func _ready() -> void:
+	pass
+
+
+func _process(_delta: float) -> void:
+	pass
+
+
+## public methods
+## private methods
