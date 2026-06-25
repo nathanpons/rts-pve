ab973158feb7cb5e0ec8a8f9740242b043d8ffb9
PapsBurr <npons3932@gmail.com>
2026-06-24 21:55:01 -0600

    refactor: change 'health' to 'curr_health' for clearer variable names

--- scripts/components/health_component.gd
+++ scripts/components/health_component.gd
@@ -4,24 +4,32 @@ extends Node2D
 @export var min_health: float = 0.0
 @export var max_health: float = 100.0
 @export var health_bar: ProgressBar
-var health: float
+var curr_health: float
 
 
 func _ready() -> void:
-	health = max_health
+	curr_health = max_health
 	health_bar.min_value = min_health
 	health_bar.max_value = max_health
-	health_bar.value = health
+	health_bar.value = curr_health
 
 
 func _process(_delta: float) -> void:
 	rotation = -owner.rotation
 
 
+func get_max_health() -> float:
+	return max_health
+
+
+func set_max_health(new_max_health: float) -> void:
+	max_health = new_max_health
+
+
 func take_damage(damage_amount: float):
-	health -= damage_amount
-	health_bar.value = health
+	curr_health -= damage_amount
+	health_bar.value = curr_health
 	print("Health: " + str(self.health))
 
-	if health <= 0:
+	if curr_health <= 0:
 		get_parent().queue_free()
