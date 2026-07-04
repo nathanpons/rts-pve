e9a01a6b4e7a7ae2706150a0e4485db18147f714
PapsBurr <npons3932@gmail.com>
2026-07-04 13:48:15 -0600

    feat: add wisp unit

---
+++ scripts/wisp_unit.gd
@@ -0,0 +1,13 @@
+extends Unit
+## signals
+## enums
+## consts
+## exports
+## public vars
+## private vars
+## onready vars
+
+## public methods
+## private methods
+func _adjust_rotation_to_match_direction() -> void:
+	pass
