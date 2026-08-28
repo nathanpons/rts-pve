c7049a58840ffbbfc31a663f32f4402967e47624
PapsBurr <npons3932@gmail.com>
2026-08-28 07:16:27 -0600

    feat: add beginning for the base projectile

---
+++ scripts/projectile_base.gd
@@ -0,0 +1,22 @@
+class_name ProjectileBase
+extends Node
+## signals
+## enums
+## consts
+## exports
+## public vars
+## private vars
+## onready vars
+## built-in override methods
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+## public methods
+
+## private methods
