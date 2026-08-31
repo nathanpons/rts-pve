b7e1405c09971a2edfcf3ee3e23137d53acfc1db
PapsBurr <npons3932@gmail.com>
2026-08-31 11:02:39 -0600

    feat: add infrastructure for projectiles to function

--- scripts/projectile_base.gd
+++ scripts/projectile_base.gd
@@ -1,22 +1,26 @@
 class_name ProjectileBase
-extends Node
+extends Node2D
 ## signals
 ## enums
 ## consts
 ## exports
+@export var speed: float = 30.0
+@export var damage: float = 20.0
+
 ## public vars
+var direction: Vector2 = Vector2.RIGHT
 ## private vars
 ## onready vars
+@onready var attack_shape: CollisionShape2D = $Area2D/CollisionShape2D
+
 ## built-in override methods
-
-
-func _ready() -> void:
-	pass
-
-
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	global_position += direction * speed * delta
 
 ## public methods
-
 ## private methods
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+
+	queue_free()
