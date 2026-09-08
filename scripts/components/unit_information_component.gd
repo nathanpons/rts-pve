58dc861c2201c0eb7e2a249b3293521b5a11c094
PapsBurr <npons3932@gmail.com>
2026-09-07 18:24:40 -0600

    feat: add unit information labels above units

---
+++ scripts/components/unit_information_component.gd
@@ -0,0 +1,44 @@
+class_name UnitInformationComponent
+extends Control
+## signals
+## enums
+## consts
+## exports
+@export var team_colors: Array[Color] = [
+	Color.BLUE,
+	Color.RED,
+	Color.YELLOW,
+	Color.GREEN,
+	Color.PURPLE,
+]
+
+## public vars
+## private vars
+## onready vars
+@onready var team_label: Label = $VBoxContainer/InfoBox/TeamLabel
+@onready var speed_label: Label = $VBoxContainer/InfoBox/SpeedLabel
+@onready var health_label: Label = $VBoxContainer/InfoBox/HealthLabel
+
+## built-in override methods
+## public methods
+func setup(team: int, speed: float, current_health: int) ->  void:
+	update_team(team)
+	update_speed(speed)
+	update_health(current_health)
+
+
+func update_team(team: int) -> void:
+	team_label.text = "Team:%sd" % team
+	if team >= 0 and team < team_colors.size():
+		team_label.modulate = team_colors[team]
+
+
+func update_speed(speed: float) -> void:
+	speed_label.text = "SPD:%d" % speed
+
+
+func update_health(health: int) -> void:
+	health_label.text = "HP:%d" % health
+
+
+## private methods
